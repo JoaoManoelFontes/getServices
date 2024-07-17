b6b25ea5caf1a8b992428d68b54d83e710c226bb
JoaoManoelFontes <jmanufontesc@gmail.com>
2024-07-17 11:49:29 -0300

    feat: create django apps

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "usuarios",
+    "servicos",
+    "agendamentos",
+    "avaliacoes",
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("usuarios/", include("usuarios.urls")),
+    path("servicos/", include("servicos.urls")),
+    path("agendamentos/", include("agendamentos.urls")),
+    path("avaliacoes/", include("avaliacoes.urls")),
 ]
