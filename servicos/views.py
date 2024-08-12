02bd35ad123efb8f60e7b081aa2e5fbf2cbee928
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-08-12 09:42:57 -0300

    fix: correct select related

--- servicos/views.py
+++ servicos/views.py
@@ -6,7 +6,7 @@ from usuarios.models import Profissional
 
 # Create your views here.
 def pagina_inicial(request: HttpRequest) -> HttpResponse:
-    profissionais = Profissional.objects.all().select_related()
+    profissionais = Profissional.objects.all().select_related("user", "servico")
 
     return render(
         request,
