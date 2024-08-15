37d0377de75b167f9be613989f4ae7a0d558c324
Thiago Morais Silva <114741077+thiagomoraiis@users.noreply.github.com>
2024-08-14 22:01:22 -0300

    Feature/adicionar foto ao perfil (#16)
    * chore: add settings to handle media files
    
    * feat: add profile_image field to BaseUser model

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = "usuarios.BaseUser"
 # Default primary key field type
--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path("", include("servicos.urls")),
     path("agendamentos/", include("agendamentos.urls")),
     path("avaliacoes/", include("avaliacoes.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
