0fe1814fa816cab907076582143613039b439015
LucasNBS <98529146+lucasNBS@users.noreply.github.com>
2024-08-16 16:16:29 -0300

    feat: add home page (#18)
    * feature: Homepage layout and responsiveness
    
    * styles: replace custom classes with daisyui and reduce the size of the icons
    
    * style: Fixes in tailwind classes and star icons
    
    * feat: remove input from stars (rating)
    
    ---------
    
    Co-authored-by: Ruan Gustavo <dev.ruangustavo@gmail.com>

---
+++ static/js/aside.js
@@ -0,0 +1,21 @@
+{
+  const openButton = document.querySelector("[data-menu-button-open]");
+  const background = document.querySelector("[data-menu-background]");
+  const closeButton = document.querySelector("[data-menu-close]");
+  const menu = document.querySelector("[data-menu]");
+
+  openButton.addEventListener("click", handleOpenMenu);
+
+  background.addEventListener("click", handleCloseMenu);
+  closeButton.addEventListener("click", handleCloseMenu);
+
+  function handleOpenMenu() {
+    background.classList.remove("hidden");
+    menu.classList.add("translate-x-[320px]");
+  }
+
+  function handleCloseMenu() {
+    background.classList.add("hidden");
+    menu.classList.remove("translate-x-[320px]");
+  }
+}
