66fc691b7c51d7673ffecb279ec89f1e08b91e5f
Ruan Gustavo <dev.ruangustavo@gmail.com>
2024-08-20 09:24:30 -0300

    feat: list professionals in home page and add description to profissional

--- servicos/views.py
+++ servicos/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
@@ -5,12 +6,14 @@ from usuarios.models import Profissional
 
 
 def pagina_inicial(request: HttpRequest) -> HttpResponse:
-    profissionais = Profissional.objects.all().select_related("user", "servico")
+    profissionais = (
+        Profissional.objects.all()
+        .select_related("user", "servico")
+        .annotate(media_avaliacao=Avg("avaliacao__nota"))
+    )
 
     return render(
         request,
         "index.html",
-        {
-            "profissionais": profissionais,
-        },
+        {"profissionais": profissionais},
     )
