517cda848af814cf3272669172722bd3284a47c9
Ruan Gustavo <dev.ruangustavo@gmail.com>
2024-08-26 12:31:53 -0300

    feat: add filtering options to the home page

--- servicos/views.py
+++ servicos/views.py
@@ -1,52 +1,59 @@
-from django.db.models import Avg
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.db.models import Avg, Q
+from django.views.generic import ListView
 
 from agendamentos import selectors
 from servicos.models import Servico
 from usuarios.models import Profissional
 
 
-def pagina_inicial(request: HttpRequest) -> HttpResponse:
-    profissionais = (
-        Profissional.objects.all()
-        .select_related("user", "servico")
-        .annotate(media_avaliacao=Avg("avaliacao__nota"))
-    )
+class PaginaInicialView(ListView):
+    model = Profissional
+    template_name = "index.html"
+    context_object_name = "profissionais"
 
-    servicos = Servico.objects.all()
-    agendamentos = []
-    horarios = []
-    mostrar_cards_profissional = False
-
-    if verificar_user_profissional(request):
-        mostrar_cards_profissional = True
-        schedule_data = selectors.get_profissional_schedule(request.user.profissional)
-        horarios = schedule_data["horarios"]
-        agendamentos = schedule_data["agendamentos"]
-
-    return render(
-        request,
-        "index.html",
-        {
-            "profissionais": profissionais,
-            "servicos": servicos,
-            "agendamentos": agendamentos,
-            "horarios": horarios,
-            "mostrar_cards_profissional": mostrar_cards_profissional,
-        },
-    )
-
-
-def verificar_user_profissional(request):
-    """Verifica se há um usuário logado e se é um profissional"""
-    if request.user.is_authenticated:
-        profissional_logado = (
-            Profissional.objects.filter(user=request.user)
-            .select_related("servico")
-            .first()
+    def get_queryset(self):
+        queryset = (
+            Profissional.objects.all()
+            .select_related("user", "servico")
+            .annotate(media_avaliacao=Avg("avaliacao__nota"))
         )
 
-        return profissional_logado
+        servico_id = self.request.GET.get("servico")
+        if servico_id and servico_id != "todos":
+            servico = Servico.objects.get(id=servico_id)
+            queryset = queryset.filter(servico=servico)
 
-    return False
+        nome_profissional = self.request.GET.get("profissional")
+        if nome_profissional:
+            queryset = queryset.filter(
+                Q(user__first_name__icontains=nome_profissional)
+                | Q(user__last_name__icontains=nome_profissional)
+            )
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["servicos"] = Servico.objects.all()
+        context["agendamentos"] = []
+        context["horarios"] = []
+        context["is_profissional_autenticado"] = False
+
+        profissional_autenticado = self.get_profissional_autenticado()
+
+        if profissional_autenticado:
+            context["is_profissional_autenticado"] = True
+            schedule_data = selectors.get_profissional_schedule(profissional_autenticado)
+            context["horarios"] = schedule_data["horarios"]
+            context["agendamentos"] = schedule_data["agendamentos"]
+
+        return context
+
+    def get_profissional_autenticado(self):
+        return (
+            Profissional.objects.filter(user=self.request.user)
+            .select_related("servico")
+            .first()
+            if self.request.user.is_authenticated
+            else None
+        )
