cd310ca072965b56b4c8ee05215e98ff51ba9c73
Ruan Gustavo <dev.ruangustavo@gmail.com>
2024-09-11 09:36:20 -0300

    feat: load environment variables

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- manage.py
+++ manage.py
@@ -3,21 +3,13 @@
 
 import os
 import sys
-from pathlib import Path
-
-import dotenv
 
 
 def main() -> None:
     """Run administrative tasks."""
     # Load environment variables from .env file
-    dotenv_path = Path(__file__).parent / ".env"
-    if dotenv_path.exists():
-        dotenv.load_dotenv(dotenv_path)
-    else:
-        raise Exception("No .env file found!")  # noqa: EM101 TRY003 TRY002
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
